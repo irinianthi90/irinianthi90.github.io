0d20de78aa4b1d3f3d88646ad8cb6542a7938853
irinianthi90 <irinianthi90@gmail.com>
2026-03-25 11:56:17 +0000

    Deploying to gh-pages from @ irinianthi90/irinianthi90.github.io@fd1675943d2bdd85d54f197f454735786f2b58c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-team",
+          title: "Team",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/team/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
