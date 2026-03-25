50df54071c5a32edbbf9dc62944d945bdade1b43
irinianthi90 <irinianthi90@gmail.com>
2026-03-25 11:17:39 +0000

    Deploying to gh-pages from @ irinianthi90/irinianthi90.github.io@2facedeb39ee08f429c1e886bdff4bf703c6259b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "irinianthi90/my-site",
+    "data-repo": "irinianthi90/irinianthi90.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
